7bdb021e602d816c78b6afcde86a3c068816e75d
Frederick Lazo <fredlazo@yahoo.com>
2016-06-12 00:10:30 -0700

    Assignment 26 completed

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,3 +1,10 @@
 class WelcomeController < ApplicationController
+  #def index
+  #end
 
+  #def about
+  #end
+
+  #def faq
+  #end
 end
--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -20,4 +20,10 @@ RSpec.describe WelcomeController, type: :controller do
     end
   end
 
+    describe "GET faq" do
+    it "renders the about template" do
+      get :faq
+      expect(response).to render_template("faq")
+    end
+  end
 end
