e744554fd92409b3997344246038a2f79adc715a
Frederick Lazo <fredlazo@yahoo.com>
2016-06-19 11:59:14 -0700

    Checkpoint 45: Public Profiles completed

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,23 +8,28 @@ class User < ActiveRecord::Base
   before_save { self.email = email.downcase if email.present? }
   before_save { self.role ||= :member }
 
-# #3
+
   validates :name, length: { minimum: 1, maximum: 100 }, presence: true
-# #4
- validates :password, presence: true, length: { minimum: 6 }, unless: :password_digest
- validates :password, length: { minimum: 6 }, allow_blank: true
-# #5
+
+  validates :password, presence: true, length: { minimum: 6 }, unless: :password_digest
+  validates :password, length: { minimum: 6 }, allow_blank: true
+
   validates :email,
             presence: true,
             uniqueness: { case_sensitive: false },
             length: { minimum: 3, maximum: 254 }
 
-# #6
+
   has_secure_password
   enum role: [:member, :admin]
 
   def favorite_for(post)
     favorites.where(post_id: post.id).first
   end
+
+    def avatar_url(size)
+      gravatar_id = Digest::MD5::hexdigest(self.email).downcase
+      "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}"
+    end  
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   resources :labels, only: [:show]
 
   resources :topics do
@@ -9,13 +10,13 @@ Rails.application.routes.draw do
 
     resources :comments, only: [:create, :destroy]
     resources :favorites, only: [:create, :destroy]
-    
+
     post '/up-vote' => 'votes#up_vote', as: :up_vote
     post '/down-vote' => 'votes#down_vote', as: :down_vote
 
   end
 
-  resources :users, only: [:new, :create]
+  resources :users, only: [:new, :create, :show]
 
   resources :sessions, only: [:new, :create, :destroy]
 
