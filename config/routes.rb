653b65640cc610150eeff6713e5f00a8c3a41b14
Frederick Lazo <fredlazo@yahoo.com>
2016-06-20 01:44:52 -0700

    Checkpoint49: Building an API completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-Rails.application.routes.draw do
+ Rails.application.routes.draw do
 
   resources :labels, only: [:show]
 
@@ -28,8 +28,8 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :users, only: [:index, :show]
-      resources :topics, only: [:index, :show]      
+      resources :users, only: [:index, :show, :create, :update]
+      resources :topics, except: [:edit, :new]
     end
   end
 
