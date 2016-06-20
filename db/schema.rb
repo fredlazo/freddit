464b368db917b62a567ec22944930fd1cf7cc55a
Frederick Lazo <fredlazo@yahoo.com>
2016-06-20 00:13:15 -0700

    Checkpoint 48: Building an API completed

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160619075028) do
+ActiveRecord::Schema.define(version: 20160620064311) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string   "title"
@@ -111,6 +111,7 @@ ActiveRecord::Schema.define(version: 20160619075028) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.integer  "role"
+    t.string   "auth_token"
   end
 
   create_table "votes", force: :cascade do |t|
