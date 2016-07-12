98ea92ae6675c68b1fbaa0e65c410a213060e85b
Frederick Lazo <fredlazo@yahoo.com>
2016-07-12 09:37:31 -0700

    Topics CRUD

---
+++ app/controllers/topics_controller.rb
@@ -0,0 +1,62 @@
+class TopicsController < ApplicationController
+  def index
+    @topics = Topic.all
+  end
+
+  def new
+    @topic = Topic.new
+  end
+
+  def create
+    @topic = Topic.new
+    @topic.name = params[:topic][:name]
+    @topic.description = params[:topic][:description]
+    @topic.public = params[:topic][:public]
+
+    if @topic.save
+      flash[:notice] = "Saved"
+      redirect_to @topic
+    else
+      flash.now[:alert] = "Fail"
+      render :new
+    end
+  end
+
+  def show
+    @topic = Topic.find(params[:id])
+  end
+
+  def edit
+    @topic = Topic.find(params[:id])
+  end
+
+  def update
+    @topic = Topic.find(params[:id])
+    @topic.name = params[:topic][:name]
+    @topic.description = params[:topic][:description]
+    @topic.public = params[:topic][:public]
+
+    if @topic.save
+      flash[:notice] = "Saved"
+      redirect_to @topic
+    else
+      flash.now[:alert] = "Fail"
+      render :edit
+    end
+  end
+
+  def destroy
+    @topic = Topic.find(params[:id])
+
+    if @topic.destroy
+      flash[:notice] = "Deleted"
+      redirect_to topics_path
+    else
+      flash:now[:alert] = "Fail"
+      render :show
+    end
+
+  end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
 
   root 'welcome#index'
 
-  resources :posts
+  resources :topics do
+    resources :posts, except: [:index]
+  end
 
   get 'about' => 'welcome#about'
 
